dc97e23118852f4c1d7c49914c6f6559b108fd7c
truonghongnhien <63738841+truonghongnhien@users.noreply.github.com>
2020-08-22 09:46:45 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('fetch', function(event) {
   var url = event.request.url;
   console.log('SW: fetch', url);
-  console.log(event);
+  console.log(event.request.headers.keys());
   if (/\.mp4$/.test(url)) {
     url = 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4';
     var options = {
