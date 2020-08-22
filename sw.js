a9797eab34c19575a8db145528e9ffbe55268ab3
truonghongnhien <63738841+truonghongnhien@users.noreply.github.com>
2020-08-22 13:34:18 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,13 +6,13 @@ self.addEventListener('fetch', function(event) {
     url = 'https://ws.hdv.fun/0/s0/LOWk9tF9CmgYmdwf5coN-lwthoifTkzwKyA3KwqJjgzxK21N9DBBr_STfHMuheQchAoTqHs3GWC1as0iitni5fxWYIgddQQp66OI-I6VnoxU0umHymkhKZnguEaXOU2JwpLIWkT6T1QOWQ6I7jRyPFLplthq33vfPSxt3vYNhgRCT1kfk3gK09gx15Zztj3Vu7djQcsxtr2TJCCjzpKfgdMb5Bq4_DnVT5hCCugZ7y-uDe1FKPM4FZz2BrJ2TbOXhKhR16Dv5VP-0_RCSZ6BFg==';
     url='https://webtorrent.io/torrents/Sintel/Sintel.mp4'
     var options = {
-      method: 'GET'
-      
-   //   headers:new Headers({'range': 'bytes=0-'})
+      method: 'GET',      
+      headers:event.request.headers
     };
-    if(event.request.headers.get('range')){
-    options.headers=new Headers({'range': event.request.headers.get('range')});
-    }
+ //   if(event.request.headers.get('range')){
+//    options.headers=new Headers({'range': event.request.headers.get('range')});
+//      event.request.headers
+//    }
     console.log(options)
     event.respondWith(fetch(url, options));
   }
