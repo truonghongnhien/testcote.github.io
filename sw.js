f8c1a0ce14a89b92440e38c2229005866b0ba776
truonghongnhien <63738841+truonghongnhien@users.noreply.github.com>
2020-08-23 00:25:15 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,6 +12,11 @@ self.addEventListener('fetch', function(event) {
   var url = event.request.url;
   var request = event.request;
   console.log('SW: fetch', url);
+  
+      console.log('!!!!proxy forward!!!!');
+        event.respondWith(fetch(request));
+        return;
+  
     console.log(event.request.headers.get('range'));
   if (/\.mp4$/.test(url)) {
     url = 'https://ws.hdv.fun/0/s0/LOWk9tF9CmgYmdwf5coN-lwthoifTkzwKyA3KwqJjgzxK21N9DBBr_STfHMuheQchAoTqHs3GWC1as0iitni5fxWYIgddQQp66OI-I6VnoxU0umHymkhKZnguEaXOU2JwpLIWkT6T1QOWQ6I7jRyPFLplthq33vfPSxt3vYNhgRCT1kfk3gK09gx15Zztj3Vu7djQcsxtr2TJCCjzpKfgdMb5Bq4_DnVT5hCCugZ7y-uDe1FKPM4FZz2BrJ2TbOXhKhR16Dv5VP-0_RCSZ6BFg==';
@@ -26,6 +31,7 @@ self.addEventListener('fetch', function(event) {
 //    }
     console.log(options)
     event.respondWith(fetch(url, options));
+    return;
   }
   else{
     console.log('proxy forward!!!!');
